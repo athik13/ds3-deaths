0b98eead80884ca919f4aeb8075339cf4860baf2
Mohamed Athik <athik.13@gmail.com>
2020-09-28 23:30:35 +0800

    layout fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="mt-20 mb-20">
+                <div class="flex flex-col align-center mt-20 mb-20">
                     <p class="text-6xl text-center">GitGud</p>
-                    <img src="/memes/2a8yr9.jpg" alt="">
+                    <img src="/memes/2a8yr9.jpg" class="mr-15 ml-15" alt="">
                 </div>
             </div>
         </div>
